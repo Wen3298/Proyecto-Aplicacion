5f40f9d1741e6a19701ceec9152f71e3e3a0ee48
wen3298 <wzelayavega@gmail.com>
2024-05-09 15:46:14 -0600

    pagina principal arreglos

--- Proyecto.js
+++ Proyecto.js
@@ -1,48 +1,51 @@
-const Name = document.getElementById("name")
-const correo = document.getElementById("email")
-const contrasenia = document.getElementById("password")
-const Signupform = document.getElementById("signupform")
-const Loginform = document.getElementById("loginform")
-const btnIngresar = document.getElementById("btnIngresar")
-const emailLogin = document.getElementById("emailLogin")
-const passwordLogin = document.getElementById("passwordLogin")
-
-let nombreLocalStorage
-let CorreoLocalStorage
-let ContraseniaLocalStorage
-
-try {
-    Signupform.addEventListener("submit" ,(e)=>{
-        e.preventDefault()
-        localStorage.setItem("nombre", Name.value);
-        localStorage.setItem("correo", email.value);
-        localStorage.setItem("Contraseña",password.value);
-        nombreLocalStorage = localStorage.getItem("nombre");
-    
-        
-      if (Name.value == nombreLocalStorage){
-        return alert ("Usuario creado Exitosamente ✅")
-      }
-    })
-    
-} catch (error) {
-    console.log("Error",error);
-}
-    
 
 
-CorreoLocalStorage = localStorage.getItem("correo")
-ContraseniaLocalStorage = localStorage.getItem("Contraseña")
-function ingresar(){
-    if(emailLogin.value == CorreoLocalStorage && passwordLogin.value == ContraseniaLocalStorage ){
-        window.location.href = "index.html"
-    }else{
-        alert("Contraseña Incorrecta ❌")
-        console.log(emailLogin.value)
-        console.log(passwordLogin.value)
-        console.log(CorreoLocalStorage)
-        console.log(ContraseniaLocalStorage)
-    }
+// const Name = document.getElementById("name")
+// const correo = document.getElementById("email")
+// const contrasenia = document.getElementById("password")
+// const Signupform = document.getElementById("signupform")
+// const Loginform = document.getElementById("loginform")
+// const btnIngresar = document.getElementById("btnIngresar")
+// const emailLogin = document.getElementById("emailLogin")
+// const passwordLogin = document.getElementById("passwordLogin")
+
+// let nombreLocalStorage
+// let CorreoLocalStorage
+// let ContraseniaLocalStorage
+
+// try {
+//     Signupform.addEventListener("submit" ,(e)=>{
+//         e.preventDefault()
+//         localStorage.setItem("nombre", Name.value);
+//         localStorage.setItem("correo", email.value);
+//         localStorage.setItem("Contraseña",password.value);
+//         nombreLocalStorage = localStorage.getItem("nombre");
 
 
-}
+//       if (Name.value == nombreLocalStorage){
+//         return alert ("Usuario creado Exitosamente ✅")
+//       }
+//     })
+
+// } catch (error) {
+//     console.log("Error",error);
+// }
+
+
+
+// CorreoLocalStorage = localStorage.getItem("correo")
+// ContraseniaLocalStorage = localStorage.getItem("Contraseña")
+// function ingresar(){
+//     if(emailLogin.value == CorreoLocalStorage && passwordLogin.value == ContraseniaLocalStorage ){
+//         window.location.href = "index.html"
+//     }else{
+//         alert("Contraseña Incorrecta ❌")
+//         console.log(emailLogin.value)
+//         console.log(passwordLogin.value)
+//         console.log(CorreoLocalStorage)
+//         console.log(ContraseniaLocalStorage)
+//     }
+
+
+// }
+
